999b4948f2096cd0c8b7365edd8d2cee531afe58
Arthur Zall <zallarthur@gmail.com>
2024-12-28 14:33:36 -0300

    function for handle form

--- refatorando.js
+++ refatorando.js
@@ -13,4 +13,21 @@ function initializeShoppingList() {
 
 }
 
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const inputItem = document.querySelector("#text-item")
+    const list = document.querySelector("ul")
+    const itemName =  inputItem?.value.trim()
+
+    if(!itemName) {
+        console.log("Erro ao adicionar novo item. Verifique se os campos foram preenchidos corretamente.")
+        return;
+    }
+
+    const newListItem = createNewItem(itemName)
+    list.appendChild(newListItem)
+    
+}
+
 initializeShoppingList()
