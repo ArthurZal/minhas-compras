03b6af0b9a46c1480e7bb38439702658e4225f85
Arthur Zall <zallarthur@gmail.com>
2024-12-28 16:16:08 -0300

    function for create new item

--- refatorando.js
+++ refatorando.js
@@ -18,16 +18,51 @@ function handleFormSubmit(event) {
 
     const inputItem = document.querySelector("#text-item")
     const list = document.querySelector("ul")
-    const itemName =  inputItem?.value.trim()
+    const itemName =  inputItem.value.trim()
 
     if(!itemName) {
-        console.log("Erro ao adicionar novo item. Verifique se os campos foram preenchidos corretamente.")
+        alert("Erro ao adicionar novo item. Verifique se os campos foram preenchidos corretamente.")
         return;
     }
 
     const newListItem = createNewItem(itemName)
     list.appendChild(newListItem)
+
+    inputItem.value = ''
     
+}
+
+function createNewItem(itemName) {
+    const li = document.createElement('li')
+
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.addEventListener('change', checkboxChange)
+
+    const span = document.createElement('span')
+    span.innerText = itemName
+
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText = 'Delete'
+
+    li.appendChild(checkbox)
+    li.appendChild(span)
+    li.appendChild(deleteButton)
+
+    return li;
+}
+
+function checkboxChange(event) {
+    const span = event.target.closest('li')?.querySelector('span')
+
+    if(event.target.checked) {
+        span.classList.add('list-item-effect')
+    
+    } else {
+        span.classList.remove('list-item-effect')
+    }
+
+
 }
 
 initializeShoppingList()
